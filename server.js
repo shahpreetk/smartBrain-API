258cc70e38f0038dbf2ffedc8e6e60d454e852be
shahpreetk <preet25@somaiya.edu>
2020-05-13 12:16:34 +0530

    change in /

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('it is working!');
 })
 
 app.post('/signin', signin.handleSignin(bcrypt,db))
