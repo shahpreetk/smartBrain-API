e543a744cdef13bd7addb90ae8a2c4c2c26536b2
shahpreetk <preet25@somaiya.edu>
2020-05-13 15:51:08 +0530

    connecting database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'preetshah',
-      password : '',
-      database : 'smart-brain'
+      connectionString : process.env.DATABSE_URL,
+      ssl : 'true'
     }
   });
 
